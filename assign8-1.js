9ae9953f7d1a7bb0a4716b535b756613aee153f9
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-04 16:20:25 +0530

    Update assign8-1.js

--- assign8-1.js
+++ assign8-1.js
@@ -1,3 +1,14 @@
-var m=require('./myModule');
-console.log(m.isPrime(98));
-console.log(m.isPrime(97));
+//declaring a variable to the function to know the number is prime or not
+var isPrime=function(n){
+  var factors=0;
+  for(var i=n-1;i>1;i--)
+    if(n%i==0)
+    {
+      factors++;
+      break;
+    }
+  return factors==0;
+}
+  
+console.log(m.isPrime(98));//displays output to the console
+console.log(m.isPrime(97));//displays output to the console
